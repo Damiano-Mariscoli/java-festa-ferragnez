a07c85350f3e33013ab6646bfd62bb65638b676f
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2025-02-27 13:46:24 +0100

    implementazione con metodo for

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,5 +1,30 @@
 package com.ferragnez.party;
 
-public class CheckGuest {
+import java.util.Scanner;
 
+public class CheckGuest {
+    public static void main(String[] args){
+        String[] arrayInvitati = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"} ;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Inserisci il tuo nome:");
+        String nome = input.nextLine();
+        input.close();
+        Boolean isOnTheList = false;
+        // System.out.println(nome);
+        for (int i = 0; i < arrayInvitati.length; i++){
+            System.out.println(arrayInvitati[i]);
+            if (arrayInvitati[i].equals(nome) ){
+                isOnTheList = true;
+                break;
+            }
+            
+        }
+        if(isOnTheList){
+            System.out.println("Sei sulla lista e puoi entrare!");
+        }else{
+            System.out.println("Non sei sulla lista mi dispiace");
+        }
+        
+    }
 }
