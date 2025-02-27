fb100ccc461411dda0710819f0cfafa9eab35ed2
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2025-02-27 13:53:25 +0100

    implementazione con ciclo while

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -11,20 +11,29 @@ public class CheckGuest {
         String nome = input.nextLine();
         input.close();
         Boolean isOnTheList = false;
+        int i = 0;
         // System.out.println(nome);
-        for (int i = 0; i < arrayInvitati.length; i++){
-            System.out.println(arrayInvitati[i]);
-            if (arrayInvitati[i].equals(nome) ){
-                isOnTheList = true;
-                break;
-            }
+        // for (i = 0; i < arrayInvitati.length; i++){
+        //     System.out.println(arrayInvitati[i]);
+        //     if (arrayInvitati[i].equals(nome) ){
+        //         isOnTheList = true;
+        //         break;
+        //     }
             
+        // }
+         
+        while(i < arrayInvitati.length){
+            System.out.println(arrayInvitati[i]);
+            if(arrayInvitati[i].equals(nome) ){
+                isOnTheList = true;
+            }
+            i ++;
         }
+
         if(isOnTheList){
             System.out.println("Sei sulla lista e puoi entrare!");
         }else{
             System.out.println("Non sei sulla lista mi dispiace");
         }
-        
     }
 }
